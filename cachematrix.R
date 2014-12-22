71784a69d61bda97b91c3d9490c1b5a83fca72fe
unknown <tejaarief@gmail.com>
2014-12-22 02:54:22 +0200

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## functions makeCacheMatrix do :
+## construct a matrix
+## check dimension of matrix
+## If they have similar dimension, then the matrix will be saved
+## Otherwise a message will be appeared
 
 makeCacheMatrix <- function(x = matrix()) {
-
+xa <- dim(x)
+if (xa[1] == xa[2]){
+m <- x
+}else{
+message("matrix can't be inversed")
 }
-
+}
 
 ## Write a short comment describing this function
+## Check output from makeCacheMatrix
+## If the return is not Null, the matrix will be inversed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+m <- makeCacheMatrix(x)
+if(!is.null(m)) {
+xinv <- solve(m)
 }
+}
+
+ji <- rbind(c(12,15), c(16,19))
+jk <- cacheSolve(ji)
